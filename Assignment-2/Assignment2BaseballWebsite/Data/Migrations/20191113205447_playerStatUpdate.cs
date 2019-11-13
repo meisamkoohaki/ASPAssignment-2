e94ce7b37c28923d723ee656fd446f77bf3ae4bc
Andrew Reid <200409135@student.georgianc.on.ca>
2019-11-13 16:06:43 -0500

    Unit Test

---
+++ Assignment-2/Assignment2BaseballWebsite/Data/Migrations/20191113205447_playerStatUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment2BaseballWebsite.Data.Migrations
+{
+    public partial class playerStatUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
