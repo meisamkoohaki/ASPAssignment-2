c3d86db0d0eaab694b2d80cedcacf0c6282e7e50
Andrew Reid <200409135@student.georgianc.on.ca>
2019-11-13 17:29:02 -0500

    added what needs to be done on schedule page and added links to show once logged in

---
+++ Assignment-2/Assignment2BaseballWebsite/Data/Migrations/20191113213319_updatePlayerStats.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment2BaseballWebsite.Data.Migrations
+{
+    public partial class updatePlayerStats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlayerStats",
+                columns: table => new
+                {
+                    StatId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Hits = table.Column<int>(nullable: false),
+                    Doubles = table.Column<int>(nullable: false),
+                    Triples = table.Column<int>(nullable: false),
+                    HomeRuns = table.Column<int>(nullable: false),
+                    RunsBattedIn = table.Column<int>(nullable: false),
+                    RegisterId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerStats", x => x.StatId);
+                    table.ForeignKey(
+                        name: "FK_PlayerStats_Register_RegisterId",
+                        column: x => x.RegisterId,
+                        principalTable: "Register",
+                        principalColumn: "RegisterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerStats_RegisterId",
+                table: "PlayerStats",
+                column: "RegisterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerStats");
+        }
+    }
+}
